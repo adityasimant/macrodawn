ca5cb837f2b12598844836b32086655920cb94c1
adityasimant <adityasimant88@gmail.com>
2023-04-14 17:15:17 +0530

    Added some components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import './App.css';
 import React from 'react';
-// import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { Box } from '@mui/material';
 
-// import ExerciseDetail from './pages/ExerciseDetail';
-// import Home from './pages/Home';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
 import Navbar from './components/Navbar';
-// import Footer from './components/Footer';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
       <Navbar />
-      {/* <Routes>
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/exercise/:id" element={<ExerciseDetail />} />
       </Routes>
-      <Footer /> */}
+      <Footer />
     </Box>
   );
 }
